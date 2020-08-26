626403499617bf2df3acc49d95799c09860145cb
nehaparmar99 <48972593+nehaparmar99@users.noreply.github.com>
2020-08-26 15:20:37 +0530

    Add files via upload

---
+++ zombie.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+	int pid = fork();
+	// Parent process
+	if (pid > 0)
+		sleep(20);
+
+	// Child process
+	else
+	{
+		exit(0);
+	}
+
+	return 0;
+
+}
