4acbe0dcba276579fb94615d87e140680642bceb
nehaparmar99 <48972593+nehaparmar99@users.noreply.github.com>
2020-09-03 14:44:48 +0530

    Update Fibonacci_multithread.cpp

--- Fibonacci_multithread.cpp
+++ Fibonacci_multithread.cpp
@@ -1,31 +1,33 @@
-#include<iostream>
-#include<pthread.h>
-void* fibonacci(void*);
-using namespace std;
-int n;
-void* fibonnacci(void* arg)
-{
-int c, first = 0, second = 1, next;
-for ( c = 0 ; c <n; c++ )
-{
-if ( c <= 1 )
-next = c;
-else
-{
-next = first + second;
-first = second;
-second = next;
-}
-cout << next << endl;
-}
-}
+  
+#include <stdio.h>
+#include <unistd.h> 
 
 int main()
 {
-pthread_t t;
-cout << "Enter the number of terms of Fibonacci series you want" << endl;
-cin >> n;
-cout << "First " << n << " terms of Fibonacci series are :- " << endl;
-pthread_create (&t , NULL , fibonacci,(void*)&n);
-return 0;
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    pid_t pid = fork();
+    if(pid==0)
+    {
+        printf("In child process calculating and printing fibonacci series\n");
+        arr[0]=0;
+        arr[1]=1;
+        for(int i =2;i<n;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        for(int i =0;i<n;i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        
+    }
+    else if(pid>0){
+       printf("In parent process printing number of fibonacci sequences\n"); 
+       printf("n = %d", n);
+    }
+
+    return 0;
 }
